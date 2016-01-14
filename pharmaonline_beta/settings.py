8a2ecc1b429e26514e2e451e2f708bf6128f23a4
HenriEd <doublerico7@gmail.com>
2016-01-14 05:41:33 -0500

    -_- v. 4

--- pharmaonline_beta/settings.py
+++ pharmaonline_beta/settings.py
@@ -112,7 +112,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
